ad9463a87ab1fc2c354ffe8e7311fa169b97836c
Sidharth P <sidharthpunathil714@gmail.com>
2022-04-10 13:02:47 +0530

    data types

---
+++ 2. Data Types/datatype.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+contract datatype {
+    bool a; // default is false
+    string name = "sid";
+    int8 num1;
+    int24 num3;
+    int num4; // default is int256 
+    uint num5; // positive types only
+    address owner;
+    address payable owner1;
+}
+
--- Solidity Overview/overview.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-
-pragma solidity ^0.8.0;
-
-contract ca {
-    string public name = "sid";
-    uint public age = 20;
-}
