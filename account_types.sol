60a0a8f8f3e4c61588a1ffd9e61c55df6cbf71ed
Sidharth P <sidharthpunathil714@gmail.com>
2022-04-10 13:10:10 +0530

    account types

---
+++ account_types.sol
@@ -0,0 +1,13 @@
+
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+contract account_types {
+    string public name = "sid";
+    uint publicage = 20;
+}
+
+// contract address = account address + latest transaction count
+// external address 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4
+// contract address 0xd9145CCE52D386f254917e481eB44e9943F39138
